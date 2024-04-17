b8199e0eaef8012b0b512e38c3012f48a6b2e4bb
clairebyrne <clairebyrne@users.noreply.github.com>
2024-04-18 00:37:08 +0100

    Update file locs

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.set_page_config(
 st.title('🥾 VAM content checker')
 st.write('## A first release of an app to help validate the data captured for each walk. ')
 # get walks data
-walk_file= 'https://github.com/clairebyrne/vam_data_validation_viewer/blob/main/WALKS_17042024.csv'
+walk_file= 'https://raw.githubusercontent.com/clairebyrne/vam_data_validation_viewer/blob/main/WALKS_17042024.csv'
 walks = pd.read_csv(walk_file, usecols=['Name', 'GeneralDescription', 'GeoJson', 'ShapeName', 'StartLocationLat', 'StartLocationLng', 'EndLocationLat', 'EndLocationLng', 'CoverImage', 
 'Duration', 'Distance', 'Grading', 'Height', 'Ascent', 'Gear', 'Safety','CarparkGettingStart',	'WayMarked', 'DogsAllowed', 'Facilities', 'Accessible', 'AccessibleToilet', 'AccessibleTerrainDescription', 'NearestCarpark'])
 walks= walks.dropna(how='all')
@@ -22,7 +22,7 @@ walks= walks.dropna(how='all')
 walks = walks.dropna(subset='GeoJson')
 
 # set gpx folder location
-gpx_dir = r'https://github.com/clairebyrne/vam_data_validation_viewer/tree/main/gpx'
+gpx_dir = r'https://raw.githubusercontent.com/clairebyrne/vam_data_validation_viewer/tree/main/gpx'
 
 
 # get list of walks from data
