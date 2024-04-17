8fcb2d73029bff610e3eba6f8dc42c726ef7eef5
clairebyrne <clairebyrne@users.noreply.github.com>
2024-04-18 00:52:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ walks= walks.dropna(how='all')
 walks = walks.dropna(subset='GeoJson')
 
 # set gpx folder location
-gpx_dir = r'https://raw.githubusercontent.com/clairebyrne/vam_data_validation_viewer/tree/main/gpx'
+gpx_dir = r'https://raw.githubusercontent.com/clairebyrne/vam_data_validation_viewer/main/gpx/'
 
 
 # get list of walks from data
